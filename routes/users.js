bdb9dd2e6b31453b4a446dfb365c859aad3450ea
feat7 <vinaykhobragade99@gmail.com>
2017-09-23 09:56:10 +0530

    registration logic

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,36 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET users dashboard. */
+router.get('/dashboard', function(req, res, next) {
+  res.send('User Dashboard');
+});
+
 //GET register page
 router.get('/register', function(req, res, next) {
   res.render('auth/register');
+});
+
+//POST register page
+router.get('/register', function(req, res, next) {
+  if (req.body.email &&
+    req.body.username &&
+    req.body.password &&
+    req.body.confirm_password) {
+    var userData = {
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+    }
+    //use schema.create to insert data into the db
+    User.create(userData, function (err, user) {
+      if (err) {
+        return next(err)
+      } else {
+        return res.redirect('/dashboard');
+      }
+    });
+  }
 });
 
 module.exports = router;
